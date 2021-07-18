3eec16a8a9a39e3bac270bfe5f60cbd7620b7d05
wenushkamallikarachchi <47137116+wenushkamallikarachchi@users.noreply.github.com>
2021-07-18 13:21:31 +0530

    remove config server

--- application/config/database.php
+++ application/config/database.php
@@ -74,27 +74,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 //config server 
-// $db['default'] = array(
-// 	'dsn'	=> '',
-// 	'hostname' => 'phpmyadmin.ecs.westminster.ac.uk',
-// 	'username' => 'w1673746',
-// 	'password' => '9xg58DVM58Ff',
-// 	'database' => 'w1673746_0',
-// 	'dbdriver' => 'mysqli',
-// 	'dbprefix' => '',
-// 	'pconnect' => FALSE,
-// 	'db_debug' => (ENVIRONMENT !== 'production'),
-// 	'cache_on' => FALSE,
-// 	'cachedir' => '',
-// 	'char_set' => 'utf8',
-// 	'dbcollat' => 'utf8_general_ci',
-// 	'swap_pre' => '',
-// 	'encrypt' => FALSE,
-// 	'compress' => FALSE,
-// 	'stricton' => FALSE,
-// 	'failover' => array(),
-// 	'save_queries' => TRUE
-// );
+
 
 
 // //config local
